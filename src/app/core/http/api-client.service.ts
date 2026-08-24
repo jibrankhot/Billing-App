142eba58f86b325cb13c52a084e8aa6ddfada49d
jibrankhot <jibrankhot5@gmail.com>
2026-08-24 20:11:17 +0530

    feat: add reusable api client service

---
+++ src/app/core/http/api-client.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiClientService {
+
+  constructor(private http: HttpClient) { }
+
+  get<T>(url: string): Observable<T> {
+    return this.http.get<T>(url);
+  }
+
+  post<T>(url: string, body: unknown): Observable<T> {
+    return this.http.post<T>(url, body);
+  }
+
+  put<T>(url: string, body: unknown): Observable<T> {
+    return this.http.put<T>(url, body);
+  }
+
+  patch<T>(url: string, body: unknown): Observable<T> {
+    return this.http.patch<T>(url, body);
+  }
+
+  delete<T>(url: string): Observable<T> {
+    return this.http.delete<T>(url);
+  }
+}
