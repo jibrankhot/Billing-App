340a0b30da706ae07c5a2269ac10730e3013b724
jibrankhot <jibrankhot5@gmail.com>
2026-08-26 21:20:35 +0530

    feat: add application sidebar navigation

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { BreadcrumbComponent } from '../components/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    SidebarComponent,
+    BreadcrumbComponent
+  ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
 export class MainLayoutComponent {
 
+  isSidebarOpen = true;
+
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
